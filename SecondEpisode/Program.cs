e6d3b4d7f40328b343c3e37affae1c7fb9314225
Alper Konuralp <alper@konuralp.gen.tr>
2021-01-30 16:10:42 +0300

    Alper İle C# bölümlerde yazmış olduğumuz kodlar. 1, 2 ve 3. bölüm için.

---
+++ SecondEpisode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SecondEpisode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a; 
+            int b = default;
+            int c = 125;
+            int d = 54;
+
+            a = 5; 
+
+            d = a + b;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            
+        }
+    }
+}
---
+++ ThirdEpisode/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ThirdEpisode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int i = 12;
+
+            // Console.WriteLine($"int [{int.MinValue}, {int.MaxValue}] arasında bir değer tutabilir.");
+
+            // byte  - 1 byte
+            // short - 2 byte
+            // int   - 4 byte
+            // long  - 8 byte
+            byte b = 1;
+            short s = 2;
+            int i = 4;
+            long l = 8;
+
+            Console.WriteLine($"byte  [{byte.MinValue}, {byte.MaxValue}] arasında bir değer tutabilir. b = {b}");
+            Console.WriteLine($"short [{short.MinValue}, {short.MaxValue}] arasında bir değer tutabilir. s = {s}");
+            Console.WriteLine($"int   [{int.MinValue}, {int.MaxValue}] arasında bir değer tutabilir. i = {i}");
+            Console.WriteLine($"long  [{long.MinValue}, {long.MaxValue}] arasında bir değer tutabilir. l = {l}");
+            
+            byte c = 129;
+            byte d = 200;
+            byte e = (byte)(c + d);
+            Console.WriteLine(e);
+
+            short f = 129;
+            short g = 200;
+            short h =(short)(f + g);
+            Console.WriteLine(h);
+
+            short j = c;
+            byte k = (byte)f;
+
+
+            int m = (int)l;
+            short n = (short)l;
+            byte o = (byte)l;
+        }
+    }
+}
