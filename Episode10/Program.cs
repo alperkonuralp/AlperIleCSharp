b7ce1a47f5a557f0d7f704a627434b20a7d8c53a
Alper Konuralp <alper@konuralp.gen.tr>
2021-02-28 15:22:32 +0300

    10. bölüm

---
+++ Episode10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Episode10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true; // false
+
+            kontrol = (5 == 3);
+
+            bool gectimi = false;
+
+            // ....
+
+            gectimi = true;
+
+
+            // Eşitlik
+            Console.WriteLine(5 == 3);
+
+            // Eşit değillik
+            Console.WriteLine(5 != 3);
+
+            // Büyük 
+            Console.WriteLine(5 > 3);
+
+            // Küçük
+            Console.WriteLine(5 < 3);
+
+            // Büyük veya Eşit
+            Console.WriteLine(5 >= 3);
+
+            // Küçük veya Eşit
+            Console.WriteLine(5 <= 3);
+
+        }
+    }
+}
