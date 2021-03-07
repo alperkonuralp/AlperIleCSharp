14a779fe1f7ac07fa1cc525521b61656a035e5d6
Alper Konuralp <alper@konuralp.gen.tr>
2021-03-07 12:29:49 +0300

    12. Bölüm

---
+++ Episode12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Episode12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+
+            if (a == b)
+            {
+                System.Console.WriteLine($"a = {a}");
+                System.Console.WriteLine($"b = {b}");
+                System.Console.WriteLine("a == b");
+            }
+            else if (a < b)
+            {
+                System.Console.WriteLine("a < b");
+            }
+            else
+            {
+                System.Console.WriteLine("a > b");
+            }
+
+        }
+    }
+}
