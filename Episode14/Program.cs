8a82f7489896e88f28886ba0931cb28937248fc4
Alper Konuralp <alper@konuralp.gen.tr>
2021-03-14 15:52:00 +0300

    14. Bölüm kodları

---
+++ Episode14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Episode14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+
+			int tutulan = random.Next(0, 2);
+
+			if(tutulan == 0)
+				Console.WriteLine(tutulan + " Tura");
+			else
+				Console.WriteLine(tutulan + " Yazı");
+
+			Console.WriteLine(tutulan + (tutulan == 0 
+																		? " Tura" 
+																		: " Yazı"));
+
+			Console.WriteLine();
+
+			tutulan = random.Next(0, 100);
+
+			if (tutulan < 50)
+				Console.WriteLine(tutulan + " Tura");
+			else
+				Console.WriteLine(tutulan + " Yazı");
+
+			Console.WriteLine(tutulan + (tutulan < 50 ? " Tura" : " Yazı"));
+		}
+	}
+}
