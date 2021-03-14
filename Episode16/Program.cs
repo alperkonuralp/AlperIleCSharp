ad9d4c8e7716885093a00777fc022edab37d1c36
Alper Konuralp <alper@konuralp.gen.tr>
2021-03-14 16:40:48 +0300

    16. Bölüm

---
+++ Episode16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Episode16
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int toplam = 0, indeks = 0;
+
+			while (indeks < 10)
+			{
+				indeks++;
+				if (indeks == 5) continue;
+
+				toplam += indeks;
+				if (toplam >= 35) break;
+			}
+
+			Console.WriteLine($"Indeks = {indeks}");
+			Console.WriteLine($"Toplam = {toplam}");
+		}
+	}
+}
