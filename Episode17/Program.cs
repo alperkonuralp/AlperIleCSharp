3032f9e7682b822e902098b94d6888c0d55f0d5d
Alper Konuralp <alper@konuralp.gen.tr>
2021-03-21 12:40:06 +0300

    17. Bölüm

---
+++ Episode17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Episode17
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 1;
+
+			while(a < 10)
+			{
+				Console.WriteLine(a);
+				a++;
+			}
+
+			Console.WriteLine();
+
+			a = 1;
+
+			do
+			{
+				Console.WriteLine(a);
+				a++;
+			} while (a < 10);
+		}
+	}
+}
