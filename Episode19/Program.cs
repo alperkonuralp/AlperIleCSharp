1436bd943ac6a0da7b67ab740b658f5ce9ca6335
Alper Konuralp <alper@konuralp.gen.tr>
2021-03-28 12:35:41 +0300

    19. Bölüm Kodları

---
+++ Episode19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Episode19
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 1;
+			int b = 2;
+			int c = 3;
+
+			int toplam = a + b + c;
+
+			Console.WriteLine("Toplam = " + toplam);
+
+			Console.WriteLine("Sınıf Kaç Kişi : ");
+			int sinifMevcudu = int.Parse(Console.ReadLine());
+
+			int[] ogrenciYaslari = new int[sinifMevcudu];
+
+			for(int i = 0; i < sinifMevcudu; i++)
+			{
+				Console.WriteLine($"{i+1}. Öğrencinin Yaşı : ");
+				int yas = int.Parse(Console.ReadLine());
+				ogrenciYaslari[i] = yas;
+			}
+
+
+			toplam = 0;
+			for (int i = 0; i < sinifMevcudu; i++)
+			{
+				toplam += ogrenciYaslari[i];
+			}
+
+			int ortalama = toplam / sinifMevcudu;
+			Console.WriteLine("Ortalama Yaş = " + ortalama);
+
+		}
+	}
+}
