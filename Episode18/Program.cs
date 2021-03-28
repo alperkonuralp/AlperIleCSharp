e1cbc031295e283d2b7221dbc957849e49a5ca1b
Alper Konuralp <alper@konuralp.gen.tr>
2021-03-28 12:35:24 +0300

    18. Bölüm Kodları

---
+++ Episode18/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Episode18
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int index = 1;
+			int toplam = 0;
+
+			while(index <= 10)
+			{
+				toplam += index;
+				index++;
+			}
+
+			Console.WriteLine("Toplam = " + toplam);
+
+
+
+
+
+			toplam = 0;
+
+			for(int i = 1; i <= 10; i++)
+			{
+				toplam += i;
+			}
+
+
+			Console.WriteLine("Toplam = " + toplam);
+
+
+
+			toplam = 0;
+
+			for (int i = 2; i <= 100; i += 2)
+			{
+				toplam += i;
+			}
+
+
+			Console.WriteLine("Toplam = " + toplam);
+
+
+			toplam = 0;
+
+			for (int i = 10; i > 0; i--)
+			{
+				toplam += i;
+			}
+
+
+			Console.WriteLine("Toplam = " + toplam);
+
+		}
+	}
+}
