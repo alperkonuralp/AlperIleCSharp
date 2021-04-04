9020b53689ca7e5223809cf30323f7e773e37a7c
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-04 12:16:48 +0300

    21. bölüm kodları

---
+++ Episode21/Episode21/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Episode21
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int i = 4;
+			int j = 7;
+			string yazi = "Merhaba";
+
+			string merhabaDunya = SayilariTopla(i);
+
+			Console.WriteLine(merhabaDunya);
+		}
+
+		public static string SayilariTopla(int ii)
+		{
+			Console.WriteLine(ii);
+			Console.WriteLine("SayilariTopla Metodu Çağırıldı.");
+			string md = "Merhaba Dünya!...";
+
+			return md;
+		}
+	}
+}
