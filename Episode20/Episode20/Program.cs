aa20d3a59c583dacd862c9649d00cb4723ecac19
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-04 11:51:43 +0300

    20. Bölüm Kodları

---
+++ Episode20/Episode20/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Episode20
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] dizi = new[] { 32, 14, 8, 9, 14, 2, 0 };
+
+			// While Örneği
+			int indeks = 0;
+			while (indeks < dizi.Length)
+			{
+				Console.WriteLine(dizi[indeks]);
+				indeks++;
+			}
+
+			Console.WriteLine();
+
+			// For Örneği
+			for (int i = 0; i < dizi.Length; i++)
+			{
+				int item = dizi[i];
+				Console.WriteLine(item);
+			}
+
+
+			Console.WriteLine();
+
+			// Foreach Örneği
+			int toplam = 0;
+			foreach (int item in dizi)
+			{
+				Console.WriteLine(item);
+				toplam += item;
+			}
+
+			Console.WriteLine("Toplam = " + toplam);
+		}
+	}
+}
