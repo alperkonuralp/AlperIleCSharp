9aa0b0ca270504cbb86f671c427ff48799ce2f09
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-11 15:10:49 +0300

    25. Bölüm Kodları

---
+++ Episode25/Episode25/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Episode25
+{
+	class Program
+	{
+		private static int Topla(int a, int b, params int[] digerSayilar)
+		{
+			int toplam = a + b;
+			foreach (int item in digerSayilar)
+			{
+				toplam += item;
+			}
+
+			return toplam;
+		}
+
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Topla(2, 3));
+			Console.WriteLine(Topla(2, 3, 4));
+			Console.WriteLine(Topla(2, 3, 4, 5));
+			Console.WriteLine(Topla(2, 3, 4, 5, 6));
+			Console.WriteLine(Topla(2, 3, 4, 5, 6, 7, 8, 9, 10));
+		}
+	}
+}
