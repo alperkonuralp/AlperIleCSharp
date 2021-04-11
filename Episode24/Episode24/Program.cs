658c936658d2670178c3de96b2bf34e2f75598ae
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-11 14:56:56 +0300

    24. Bölüm Kodları

---
+++ Episode24/Episode24/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Episode24
+{
+	class Program
+	{
+		private static int Topla(int a, int b, int c = 0, int d = 0)
+		{
+			return a + b + c + d;
+		}
+
+		private static int IslemYap(int deger, bool ekranaYaz = false)
+		{
+			int sonuc = deger * deger;
+			if (ekranaYaz) Console.WriteLine("Sonuc = " + sonuc);
+			return sonuc;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Topla(2, 3));
+			Console.WriteLine(Topla(2, 3, 4));
+			Console.WriteLine(Topla(2, 3, 4, 5));
+
+			Console.WriteLine(IslemYap(5, true));
+		}
+	}
+}
