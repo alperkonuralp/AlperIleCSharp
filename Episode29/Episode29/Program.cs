0cf622f2af8e3159c59756b17c435292d9a11c9b
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-24 16:17:55 +0300

    29. bölüm kodları

---
+++ Episode29/Episode29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Episode29
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Araba araba = new Araba();
+
+			araba.Renk = "Kırmızı";
+			araba.KendiniTanit();
+		}
+	}
+
+	public class Araba
+	{
+		public string Renk;
+
+		public void KendiniTanit()
+		{
+			Console.WriteLine($"Ben bir {Renk} arabayım.");
+		}
+	}
+}
