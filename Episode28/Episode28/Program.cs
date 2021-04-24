1dd1582dfdac989b2cd089de0120600847449521
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-24 13:16:29 +0300

    28. Bölüm Kodları

---
+++ Episode28/Episode28/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Episode28
+{
+	class Program
+	{
+		private static int Topla(int a, int b)
+		{
+			return a + b;
+		}
+		
+
+		private static int Topla(int a, ref int b)
+		{
+			return a + b;
+		}
+
+
+		private static double Topla(double a, double b)
+		{
+			return a + b;
+		}
+
+
+		static void Main(string[] args)
+		{
+			int b = 3;
+			Console.WriteLine(Topla(2,ref b));
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
