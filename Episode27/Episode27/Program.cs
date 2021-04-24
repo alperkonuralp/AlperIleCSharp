00ff0d50c05538a3b975ef9903753f5644c99898
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-24 13:00:37 +0300

    27. Bölümün Kodları

---
+++ Episode27/Episode27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Episode27
+{
+	class Program
+	{
+		private static int Topla(ref int a, int b)
+		{
+			int toplam = a + b;
+			a++;
+			b++;
+			return toplam;
+		}
+
+		static void Main(string[] args)
+		{
+			int a = 3;
+			int b = 5;
+
+			int toplam = Topla(ref a, b);
+
+			Console.WriteLine($"a + b = {a} + {b} = {toplam}");
+
+		}
+	}
+}
