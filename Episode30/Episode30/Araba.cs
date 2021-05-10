c97ef7dd63e7f94b0ac7f4ef6110c2cd17a0b522
Alper Konuralp <alper@konuralp.gen.tr>
2021-05-10 17:07:55 +0300

    30. bölüm kodları

---
+++ Episode30/Episode30/Araba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Episode30
+{
+	public class Araba
+	{
+		private string renk;
+
+		public string RengiSoyle()
+		{
+			return renk;
+		}
+
+		public void RengiDegistir(string r)
+		{
+			switch (r)
+			{
+				case "Beyaz":
+				case "Siyah":
+				case "Metalik":
+					renk = r;
+					break;
+				default:
+					throw new ApplicationException("Renk Tanımsız.");
+			}
+		}
+	}
+
+	public class BinekAraba : Araba
+	{
+
+	}
+}
---
+++ Episode30/Episode30/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Episode30
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Araba araba = new Araba();
+			araba.RengiDegistir("Siyah");
+			Console.WriteLine(araba.RengiSoyle());
+		}
+	}
+}
