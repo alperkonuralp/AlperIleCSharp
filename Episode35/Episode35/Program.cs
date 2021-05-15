95190d0d36f0fe62e69367c2e954ef061564051f
Alper Konuralp <alper@konuralp.gen.tr>
2021-05-15 16:34:07 +0300

    35. Bölüm Kodları

---
+++ Episode35/Episode35/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Episode35
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Calisan c1 = new Calisan();
+			c1.Id = 1;
+			c1.Ad = "Alper";
+			c1.Soyad = "Konuralp";
+			c1.SGKId = 12345;
+			
+
+
+			Kisi k1 = new Kisi();
+
+
+			Musteri m1 = new Musteri();
+			
+		}
+	}
+
+	public class Kisi
+	{
+		public int Id { get; set; }
+
+		public string Ad { get; set; }
+
+		public string Soyad { get; set; }
+
+	}
+
+	public class Calisan : Kisi
+	{
+		public int SGKId { get; set; }
+	}
+
+	public class OtoCalisani : Calisan
+	{
+
+	}
+
+	public class Musteri : Kisi
+	{
+		public DateTime AlisVerisTarihi { get; set; }
+	}
+}
