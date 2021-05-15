743df134e25213ee4cb074548dbc5610f30b777a
Alper Konuralp <alper@konuralp.gen.tr>
2021-05-15 14:55:23 +0300

    34. Bölüm Kodları

---
+++ Episode34/Episode34/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Episode34
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			int i = default;
+
+			Console.WriteLine(i);
+
+			DateTime dateTime = default;
+
+			Console.WriteLine(dateTime);
+
+			string s = default;
+
+			Console.WriteLine(s);
+
+			Console.WriteLine(s.Length);
+		}
+	}
+}
