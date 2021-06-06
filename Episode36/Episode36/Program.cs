cee4dd9f3dd20ed2a16ef167e531f783863ea60d
Alper Konuralp <alper@konuralp.gen.tr>
2021-06-06 12:58:27 +0300

    36. Bölüm Kodları

---
+++ Episode36/Episode36/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Episode36
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Deneme deneme = new Deneme();
+			
+		}
+	}
+
+	public class Deneme
+	{
+		public int Id = 0;
+
+		protected string ad = "";
+	}
+
+	public class Deneme2 : Deneme
+	{
+		public void A()
+		{
+			//this.ad
+		}
+	}
+}
