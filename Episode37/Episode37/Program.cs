61339cd1ba321c06bfec14db99dd85fbca35c8d4
Alper Konuralp <alper@konuralp.gen.tr>
2021-06-06 14:20:29 +0300

    37. Bölüm Kodları

---
+++ Episode37/Episode37/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Episode37
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Kisi kisi = new Kisi();
+
+			Calisan calisan = new Calisan();
+			Kisi calisanKisi = calisan; // new Calisan();
+			Calisan calisan1 = (Calisan)calisanKisi;
+
+			object calisanKisiObject = calisanKisi; // Boxing
+
+			Calisan calisan2 = (Calisan)calisanKisiObject; // Unboxing
+
+		}
+	}
+
+	public class Kisi
+	{
+		public int Id = 0;
+
+		public string Ad = "";
+	}
+
+	public class Calisan : Kisi
+	{
+		public string SGKNo = "";
+	}
+}
