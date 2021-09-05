20d5138e7992c2e622cab0661a655e71740c1f9c
Alper Konuralp <alper@konuralp.gen.tr>
2021-09-05 13:01:37 +0300

    41. Bölüm Kodları

---
+++ Episode41/Episode41/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Episode41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogManager logManager = new LogManager();
+
+            logManager.LogYaz("Hello World!");
+        }
+    }
+
+    public class LogManager
+    {
+        private Logger[] _loggers = new Logger[] { 
+            new ConsoleLogger(),
+            new DebugLogger(),
+        };
+
+        public void LogYaz(string mesaj)
+        {
+            foreach (Logger logger in _loggers)
+            {
+                logger.LogYaz(mesaj);
+            }
+        }
+    }
+
+    public abstract class Logger
+    {
+        public abstract string Isim { get; }
+        public abstract void LogYaz(string mesaj);
+    }
+    public class ConsoleLogger : Logger
+    {
+        public override string Isim { get { return nameof(ConsoleLogger); } }
+        public override void LogYaz(string mesaj)
+        {
+            Console.WriteLine(mesaj);
+        }
+    }
+    public class DebugLogger : Logger
+    {
+        public override string Isim { get { return nameof(DebugLogger); } }
+        public override void LogYaz(string mesaj)
+        {
+            Debug.WriteLine(mesaj);
+        }
+    }
+}
